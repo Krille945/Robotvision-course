213f538c7f3e117fded702b25e0274d0caf6b07c
Smusy97 <129727223+Smusy97@users.noreply.github.com>
2023-04-04 14:04:00 +0200

    main

---
+++ mainRV.py
@@ -0,0 +1,14 @@
+import MV
+import RVTransform
+
+#Type=1 is 2x2
+#Type=2 is 2x4
+type = 1
+colour = 0
+
+x,y,A=MV.get_xyA(type,colour)
+Coords,Angle=RVTransform.Transform(x,y,A)
+
+print('\nThe Results are then:')
+print('Cords:'+str(Coords))
+print('Angle:'+str(Angle))
