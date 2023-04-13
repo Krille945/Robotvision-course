ec465357477db5937c8047fe5af83475c1b5dbed
Christopher Endelt <77574340+Krille945@users.noreply.github.com>
2023-04-13 20:33:30 +0200

    Update Calibration.py

--- Calibration.py
+++ Calibration.py
@@ -171,12 +171,14 @@ def get_xyA(type,colour):
                 length_mm=100 #in mm
                 length_px=RV_Math.calc_length(result_x,result_y)
                 mm_pr_px=length_mm/length_px
+                T_to_brick_x=100 #in mm
+                T_to_brick_y=100 #in mm
+                i_min=np.min(result_x) ### This should be checked
                 print('The average angle of the brick is'+str(np.mean(result_angle)))
                 print('The mm per pixel is:' + str(mm_pr_px))
-                print('The first found brick had the following coordinates [x,y] in px:' + str([result_x[0],result_y[0]]))
-                print('The second found brick had the following coordinates [x,y] in px:' + str([result_x[0],result_y[0]]))
-                print('The first found brick had the following offset from [0,0] [x,y] in mm:' + str([result_x[0]*mm_pr_px,result_y[0]*mm_pr_px]))
-                print('The second found brick had the following offset from [0,0] [x,y] in mm:' + str([result_x[0]*mm_pr_px,result_y[0]*mm_pr_px]))
+                print('The first found brick had the following coordinates [x,y] in px: ' + str([result_x[i_min],result_y[i_min]]))
+                print('The first found brick had the following offset from [0,0] [x,y] in mm: ' + str([result_x[i_min]*mm_pr_px,result_y[i_min]*mm_pr_px]))
+                print('To [0,0] then has the following transformation [x,y,C]: '+str([T_to_brick_x-result_x[i_min]*mm_pr_px,T_to_brick_y-result_y[i_min]*mm_pr_px, np.mean(result_angle)]))
 
                 Found=True
 
